48a3206e7932452ec958e23249769ec7b2e40760
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-14 23:40:40 +0100

    ADD : Final

--- MaterialDesign/PageCommandes.xaml.cs
+++ MaterialDesign/PageCommandes.xaml.cs
@@ -24,5 +24,10 @@ namespace MaterialDesign
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Commande Burger 01 !");
+        }
     }
 }
